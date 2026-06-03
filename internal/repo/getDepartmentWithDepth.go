e69a39ba4f686073c44beb227d12dc91bf242244
Evgeniy Orekhov <super-nutman@yandex.ru>
2026-06-03 20:54:52 +0300

    add order_by created_at Employees

--- internal/repo/getDepartmentWithDepth.go
+++ internal/repo/getDepartmentWithDepth.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"orgService/internal/model"
 	"strings"
+
+	"gorm.io/gorm"
 )
 
 func (r *repo) GetDepartmentWithDepth(ctx context.Context, id int, hint *model.GetParams) (*model.Department, error) {
@@ -13,17 +15,25 @@ func (r *repo) GetDepartmentWithDepth(ctx context.Context, id int, hint *model.G
 	query := r.db.WithContext(ctx).Model(model.Department{})
 
 	if hint.IncludeEmployees {
-		query.Preload("Children.Employees").Preload("Employees")
+		query.Preload("Children.Employees", func(db *gorm.DB) *gorm.DB {
+			return db.Order("created_at ASC")
+		}).
+			Preload("Employees", func(db *gorm.DB) *gorm.DB {
+				return db.Order("created_at ASC")
+			})
 
 		for i := 1; i <= hint.Depth-1; i++ {
 			prefix := strings.Repeat("Children.", i) + "Children" + ".Employees"
-			query = query.Preload(prefix)
+			query = query.Preload(prefix, func(db *gorm.DB) *gorm.DB {
+				return db.Order("created_at ASC")
+			})
 		}
 	} else {
 
 		for i := 1; i <= hint.Depth-1; i++ {
 			prefix := strings.Repeat("Children.", i) + "Children"
 			query = query.Preload(prefix)
+
 		}
 	}
 	err := query.Where("id = ?", id).Take(&department).Error
